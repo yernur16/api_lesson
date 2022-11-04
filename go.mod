17ac356b46d2f1439925e7c1ca241197f58351f3
yernur <yernur.abishev@gmail.com>
2022-11-04 07:11:21 +0600

    Started CRUD tests, errors errors errors

--- go.mod
+++ go.mod
@@ -2,10 +2,9 @@ module api
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
-
 require (
-	github.com/jmoiron/sqlx v1.3.5 // indirect
-	github.com/lib/pq v1.10.7 // indirect
-
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.7
 )
---
+++ pkg/user/user_test.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	d "api/db"
+	"encoding/json"
+	"log"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+)
+
+func Test_CRUD(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error occured '%s' expected when opening db connection", err)
+	}
+
+	testDB := sqlx.NewDb(db, "sqlmock")
+	d.DB = testDB
+
+	u := &User{
+		Data: Data{
+			First_name: "yernur_testcrud",
+			Last_name:  "abishev_testcrud",
+			Interests:  "coding,pushups,etc",
+		},
+	}
+	bytes, err := json.Marshal(u)
+	if err != nil {
+		t.Error(err)
+	}
+	mock.ExpectExec(`INSERT INTO users`).WithArgs([]byte(bytes)).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = u.CreateUser()
+	if err != nil {
+		t.Errorf("error with CreateUser method: %s", err)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("error with expectations: %s", err)
+
+	}
+
+}
